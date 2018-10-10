97dc933fcca9607d5b5ebbfaabfb44bc56f15c17
Manny Pissanos <mpissano@gmail.com>
2018-10-10 13:31:17 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,7 +3,6 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
- extend MetaDancing
  extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
  
